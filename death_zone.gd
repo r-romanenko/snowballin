868a99ff96214e85431817fbc3cb84638f48a40a
Tom Strzyz <tom564tom@gmail.com>
2024-11-09 02:07:43 -0800

    bug fix and small movement adjust

--- death_zone.gd
+++ death_zone.gd
@@ -1,6 +1,7 @@
 extends Area3D
 
+
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "Ball":
-		get_tree().reload_current_scene()
+		get_tree().call_deferred("reload_current_scene")
 	
--- victory_zone.gd
+++ victory_zone.gd
@@ -2,6 +2,10 @@ extends Area3D
 
 @export_file("*.tscn") var NEXT_LEVEL: String = ""
 
+
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "Ball":
-		get_tree().change_scene(NEXT_LEVEL)
+		call_deferred("_change_scene")
+
+func _change_scene() -> void:
+	await get_tree().change_scene_to_file(NEXT_LEVEL)
