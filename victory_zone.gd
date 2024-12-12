acd4ba982a7ccdb2cd964c847248963af45648be
Ruslan Romanenko <71728377+r-romanenko@users.noreply.github.com>
2024-12-11 20:51:48 -0800

    Working end game screen!

--- victory_zone.gd
+++ victory_zone.gd
@@ -1,28 +1,26 @@
 extends Area3D
 
 @export_file("*.tscn") var NEXT_LEVEL: String = ""
-
-@export var required_energy = 30
-
+@export var required_energy = 20
 
 func _on_body_entered(body: RigidBody3D) -> void:
 	if body.name == "Player":
 		var ball_scale = body.scale.length() # size based on scale
 		var ball_speed = body.linear_velocity.length()
-		
+
 		if ball_scale * ball_speed >= required_energy:
-			# Check the path of the current scene
-			var current_scene_path = get_tree().root.get_child(0).filename
+			var current_scene_path = get_tree().current_scene.get_scene_file_path()
+			print("Current scene path: ", current_scene_path)
 			if current_scene_path == "res://level_02.tscn":
 				get_tree().change_scene_to_file("res://EndGame.tscn")
 			else:
 				call_deferred("_change_scene")
+
 		else:
 			call_deferred("_reset_level")
 
 func _change_scene() -> void:
 	get_tree().change_scene_to_file(NEXT_LEVEL)
 
-	
 func _reset_level() -> void:
 	get_tree().reload_current_scene()
